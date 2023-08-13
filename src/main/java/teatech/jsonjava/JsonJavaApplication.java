a83f62fb42df9d4f05b21107cc17f3e84ac4cb33
Singgih Pratama <singgih.vendor.pasti@pegadaian.co.id>
2023-08-13 16:48:38 +0700

    initialize project json-java

---
+++ src/main/java/teatech/jsonjava/JsonJavaApplication.java
@@ -0,0 +1,13 @@
+package teatech.jsonjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/teatech/jsonjava/JsonJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package teatech.jsonjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
