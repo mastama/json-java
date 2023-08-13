d39dbd413ad4b2388dcfd35c492aebb61a57edc0
Singgih Pratama <singgih.vendor.pasti@pegadaian.co.id>
2023-08-13 19:04:40 +0700

    map to json

---
+++ src/test/java/teatech/jsonjava/JsonObjectTest.java
@@ -0,0 +1,29 @@
+package teatech.jsonjava;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+public class JsonObjectTest {
+
+    @Test
+    void createJson() throws JsonProcessingException {
+        Map<String, Object> person = Map.of(
+                "firstName", "Singgih",
+                "lastName", "Pratama",
+                "age", 27,
+                "address", Map.of(
+                        "city", "Depok",
+                        "country", "Indonesia",
+                        "street", "jalanin aja dulu"
+                ),
+                "email","sp.mastama@gmail.com"
+        );
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(person);
+        System.out.println(json);
+    }
+}
